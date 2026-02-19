b705e66f6fc11ec0b14069b494107c2cb71317ac
Leo <leooo0s2@icloud.com>
2026-02-19 17:35:10 -0300

    Entendendo os operadores matemáticos

--- screen-match.php
+++ screen-match.php
@@ -5,7 +5,16 @@ echo "Bem-vindo(a) ao screen match!
 
 $nomeFilme = "Top Gun - Maverick";
 $anoLancamento = 2022;
-$notaFilme = 8.8;
-$incluidoNoPlano = true;
 
-echo $nomeFilme;
+$somaDeNotas = 9;
+$somaDeNotas += 6;
+$somaDeNotas += 8;
+$somaDeNotas += 7.5;
+$somaDeNotas += 5;
+
+$notaFilme = $somaDeNotas / 5;
+$planoPrime = true;
+
+$incluidoNoPlano = $planoPrime || $anoLancamento < 2020;
+
+echo $notaFilme;
