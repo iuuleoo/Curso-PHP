100cd25fbe814e42cffa3250baf09fb0e9c4c1bc
Leo <leooo0s2@icloud.com>
2026-02-19 23:07:02 -0300

    Lista de valores

--- screen-match.php
+++ screen-match.php
@@ -4,15 +4,16 @@ echo "Bem-vindo(a) ao screen match!\n";
 
 $nomeFilme = "Top Gun - Maverick";
 
-$anoLancamento = $argv[1] ?? 2022;
+$anoLancamento = 2022;
 
-$somaDeNotas = 9;
-$somaDeNotas += 6;
-$somaDeNotas += 8;
-$somaDeNotas += 7.5;
-$somaDeNotas += 5;
+$quantidadeDeNotas = $argc - 1;
+$somaDeNotas = 0;
 
-$notaFilme = $somaDeNotas / 5;
+for ($contador = 1; $contador < $argc; $contador ++) {
+    $somaDeNotas += $argv[$contador];
+}
+
+$notaFilme = $somaDeNotas / $quantidadeDeNotas;
 $planoPrime = true;
 
 $incluidoNoPlano = $planoPrime || $anoLancamento < 2020;
@@ -36,4 +37,11 @@ $genero = match ($nomeFilme) {
     default => "gênero desconhecido"
 };
 
-echo "O gênero do filme é: $genero";
+echo "O gênero do filme é: $genero\n";
+
+$filme = [
+    "Thor:Ragnarok",
+    2021,
+    7.8,
+    "super-herói",
+];
