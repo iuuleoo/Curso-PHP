b6ea72a8f1dde630597850dc2022aab4305d7d3d
Leo <leooo0s2@icloud.com>
2026-02-19 22:42:19 -0300

    Decisões no PHP

--- screen-match.php
+++ screen-match.php
@@ -19,3 +19,11 @@ $incluidoNoPlano = $planoPrime || $anoLancamento < 2020;
 echo "Nome do filme: " . $nomeFilme . "\n";
 echo "Nota do filme: " . $notaFilme . "\n";
 echo "Ano de lançamento: " . $anoLancamento . "\n";
+
+if ($anoLancamento > 2022) {
+    echo "Esse filme é um lançamento";
+} elseif($anoLancamento > 2020 && $anoLancamento <= 2022) {
+    echo "Esse filme é recente";
+}else {
+    echo "Esse filme não é um lançamento";
+}
